16427a6da0baaa59c8946d352015b018a090065b
Albert Weichselbraun <albert.weichselbraun@htwchur.ch>
2017-12-07 20:54:09 +0100

    fix: correctly serialize mappings.

--- get-unique-mappings.py
+++ get-unique-mappings.py
@@ -26,7 +26,7 @@ def parse_file(src, dst):
             try:
                 link_text, link_target = [cell.strip() for cell in line.decode('utf-8', errors='ignore').split('\t')]
                 mapping[link_text][link_target] += 1
-                if no % CNT_INTERVALL == 0:
+                if no % CNT_INTERVALL == 0 and no != 0:
                     print("Parsed {} lines yielding {} linkings.".format(no, len(mapping)))
             except ValueError:
                 print("Error parsing line '{}'.".format(line))
@@ -34,10 +34,16 @@ def parse_file(src, dst):
     # serialize mapping
     with lzma.open(dst, 'w') as f:
         for link_text, link_targets in mapping.items():
-            if len(link_target) > 1:
+            # skip ambiguous links
+            if len(link_targets) > 1:
                 continue
 
-            f.write("{}\t{}\t{}\n".format(link_text, link_target.keys()[0], link_target.values()[0]))
+            # skip links text that is a prefix of the target (e.g. Pope for Pope Benedikt)
+            link_target = list(link_targets.keys()).pop()
+            if link_target.startswith(link_text):
+                continue
+
+            f.write("{}\t{}\t{}\n".format(link_text, link_target, list(link_targets.values()).pop()).encode('utf-8'))
 
 if __name__ == '__main__':
     parser = get_parser()
